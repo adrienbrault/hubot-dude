ffd36a74b551b514ee9f0cc609e752ef9d3b4083
Adrien Brault <adrien.brault@gmail.com>
2014-03-09 21:29:59 -0700

    Add server script for opsworks

---
+++ server.js
@@ -0,0 +1,13 @@
+var exec = require('child_process').exec,
+    child;
+
+child = exec(
+    'bin/hubot -a slack -n dude',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+    }
+);
